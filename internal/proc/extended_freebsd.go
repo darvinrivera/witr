e040233e40a935602eae31db25eaa05634ad91a7
Pranshu Parmar <pranshu.parmar@gmail.com>
2026-01-07 05:29:20 +0000

    Staging merge, windows, freebsd fixes

--- internal/proc/extended_freebsd.go
+++ internal/proc/extended_freebsd.go
@@ -1,10 +1,10 @@
-//go:build darwin
+//go:build freebsd
 
 package proc
 
 import "github.com/pranshuparmar/witr/pkg/model"
 
-// ReadExtendedInfo is a no-op on macOS for now.
+// ReadExtendedInfo is a no-op on FreeBSD for now.
 func ReadExtendedInfo(pid int) (model.MemoryInfo, model.IOStats, []string, int, uint64, []int, int, error) {
 	return model.MemoryInfo{}, model.IOStats{}, nil, 0, 0, nil, 0, nil
 }
